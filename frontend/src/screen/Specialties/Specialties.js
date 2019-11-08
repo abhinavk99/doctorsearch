63ddd1a5b6af71c87b1bfb57dc1be831507c48bc
Abhinav Kasamsetty <abhinavkasamsetty@gmail.com>
2019-11-08 17:43:30 -0600

    Made specialty table rows clickable again

--- frontend/src/screen/Specialties/Specialties.js
+++ frontend/src/screen/Specialties/Specialties.js
@@ -22,7 +22,7 @@ const columns = [
     label: 'Specialty',
     minWidth: 170,
     format: value => {
-      return <p style={{ color: 'blue', cursor: 'pointer' }}>{fmat.capitalize(value)}</p>;
+      return <p>{fmat.capitalize(value)}</p>;
     },
     className: ''
   },
@@ -181,134 +181,140 @@ class Specialties extends React.Component {
       return <div></div>;
     }
     return (
-      <Paper style={{ margin: '5em' }}>
-        <div>
-          <div style={{ margin: '1em', textAlign: 'center' }}>
-            <Grid container spacing={2} justify="center">
-              <CssTextField
-                id="outlined-basic"
-                label="Search for a specialty"
-                margin="normal"
-                variant="outlined"
-                onChange={this.handleChange}
-                onKeyDown={this.handleKey}
-                style={{ width: '60em' }}
-              />
-            </Grid>
-            <Grid container spacing={2} justify="center" style={{ marginTop: '1em' }}>
-              <FormControl>
-                <InputLabel htmlFor="category">Category</InputLabel>
-                <NativeSelect
-                  value={this.state.state}
-                  onChange={this.filterCategory}
-                  style={{ width: '20em' }}
-                  inputProps={{
-                    name: 'category',
-                    id: 'category'
-                  }}
-                >
-                  <option value={'-1'}></option> />
-                  {categories.map((category, i) => (
-                    <option value={categories[i]}>{fmat.capitalize(category)}</option>
-                  ))}
-                </NativeSelect>
-              </FormControl>
-              <FormControl>
-                <InputLabel htmlFor="sortCities">Sort by Number of Cities</InputLabel>
-                <NativeSelect
-                  value={this.state.sort}
-                  onChange={this.sortNumCities}
-                  style={{ width: '20em' }}
-                  inputProps={{
-                    name: 'sortCities',
-                    id: 'sortCities'
-                  }}
-                >
-                  <option value={'-1'}></option> />
-                  <option value={'asc'}>Ascending</option>
-                  <option value={'desc'}>Descending</option>
-                </NativeSelect>
-              </FormControl>
-              <FormControl>
-                <InputLabel htmlFor="sortDoc">Sort by Number of Doctors</InputLabel>
-                <NativeSelect
-                  value={this.state.sort}
-                  onChange={this.sortNumDoc}
-                  style={{ width: '20em' }}
-                  inputProps={{
-                    name: 'sortDoc',
-                    id: 'sortDoc'
-                  }}
-                >
-                  <option value={'-1'}></option> />
-                  <option value={'asc'}>Ascending</option>
-                  <option value={'desc'}>Descending</option>
-                </NativeSelect>
-              </FormControl>
-            </Grid>
-          </div>
-          <Table stickyHeader aria-label="sticky table">
-            <TableHead>
-              <TableRow>
-                {columns.map(column => (
-                  <TableCell
-                    key={column.id}
-                    align={column.align}
-                    style={{ minWidth: column.minWidth }}
+      <div>
+        <h2 style={{ textAlign: 'center' }}>Specialties that Doctors Practice</h2>
+        <Paper style={{ marginLeft: '5em', marginRight: '5em', marginBottom: '5em' }}>
+          <div>
+            <div style={{ margin: '1em', textAlign: 'center' }}>
+              <Grid container spacing={2} justify="center">
+                <CssTextField
+                  id="outlined-basic"
+                  label="Search for a specialty"
+                  margin="normal"
+                  variant="outlined"
+                  onChange={this.handleChange}
+                  onKeyDown={this.handleKey}
+                  style={{ width: '60em' }}
+                />
+              </Grid>
+              <Grid container spacing={2} justify="center" style={{ marginTop: '1em' }}>
+                <FormControl>
+                  <InputLabel htmlFor="category">Category</InputLabel>
+                  <NativeSelect
+                    value={this.state.state}
+                    onChange={this.filterCategory}
+                    style={{ width: '20em' }}
+                    inputProps={{
+                      name: 'category',
+                      id: 'category'
+                    }}
                   >
-                    {column.label}
-                  </TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {this.state.data.objects.map(row => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.name}>
-                    {columns.map(column => {
-                      const value = row[column.id];
-                      return (
-                        <TableCell
-                          key={column.id}
-                          align={column.align}
-                          className={column.className}
-                          onClick={
-                            column.id === 'name'
-                              ? () =>
-                                  this.props.history.push({
-                                    pathname: '/specialties/' + row.id,
-                                    search: '',
-                                    state: { data: row }
-                                  })
-                              : null
-                          }
-                        >
-                          {column.format(value)}
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
-                );
-              })}
-            </TableBody>
-          </Table>
-        </div>
-        <TablePagination
-          rowsPerPageOptions={[10]}
-          component="div"
-          count={this.state.data.num_results}
-          rowsPerPage={this.state.rowsPerPage}
-          page={this.state.page}
-          backIconButtonProps={{
-            'aria-label': 'previous page'
-          }}
-          nextIconButtonProps={{
-            'aria-label': 'next page'
-          }}
-          onChangePage={this.handleChangePage}
-          onChangeRowsPerPage={this.handleChangeRowsPerPage}
-        />
-      </Paper>
+                    <option value={'-1'}></option> />
+                    {categories.map((category, i) => (
+                      <option value={categories[i]}>{fmat.capitalize(category)}</option>
+                    ))}
+                  </NativeSelect>
+                </FormControl>
+                <FormControl>
+                  <InputLabel htmlFor="sortCities">Sort by Number of Cities</InputLabel>
+                  <NativeSelect
+                    value={this.state.sort}
+                    onChange={this.sortNumCities}
+                    style={{ width: '20em' }}
+                    inputProps={{
+                      name: 'sortCities',
+                      id: 'sortCities'
+                    }}
+                  >
+                    <option value={'-1'}></option> />
+                    <option value={'asc'}>Ascending</option>
+                    <option value={'desc'}>Descending</option>
+                  </NativeSelect>
+                </FormControl>
+                <FormControl>
+                  <InputLabel htmlFor="sortDoc">Sort by Number of Doctors</InputLabel>
+                  <NativeSelect
+                    value={this.state.sort}
+                    onChange={this.sortNumDoc}
+                    style={{ width: '20em' }}
+                    inputProps={{
+                      name: 'sortDoc',
+                      id: 'sortDoc'
+                    }}
+                  >
+                    <option value={'-1'}></option> />
+                    <option value={'asc'}>Ascending</option>
+                    <option value={'desc'}>Descending</option>
+                  </NativeSelect>
+                </FormControl>
+              </Grid>
+            </div>
+            <Table stickyHeader aria-label="sticky table">
+              <TableHead>
+                <TableRow>
+                  {columns.map(column => (
+                    <TableCell
+                      key={column.id}
+                      align={column.align}
+                      style={{ minWidth: column.minWidth }}
+                    >
+                      {column.label}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {this.state.data.objects.map(row => {
+                  return (
+                    <TableRow
+                      hover
+                      role="checkbox"
+                      tabIndex={-1}
+                      key={row.name}
+                      style={{ cursor: 'pointer' }}
+                      onClick={() =>
+                        this.props.history.push({
+                          pathname: '/specialties/' + row.id,
+                          search: '',
+                          state: { data: row }
+                        })
+                      }
+                    >
+                      {columns.map(column => {
+                        const value = row[column.id];
+                        return (
+                          <TableCell
+                            key={column.id}
+                            align={column.align}
+                            className={column.className}
+                          >
+                            {column.format(value)}
+                          </TableCell>
+                        );
+                      })}
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </div>
+          <TablePagination
+            rowsPerPageOptions={[10]}
+            component="div"
+            count={this.state.data.num_results}
+            rowsPerPage={this.state.rowsPerPage}
+            page={this.state.page}
+            backIconButtonProps={{
+              'aria-label': 'previous page'
+            }}
+            nextIconButtonProps={{
+              'aria-label': 'next page'
+            }}
+            onChangePage={this.handleChangePage}
+            onChangeRowsPerPage={this.handleChangeRowsPerPage}
+          />
+        </Paper>
+      </div>
     );
   }
 }

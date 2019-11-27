200155c0736eb90c196a4f117b688b01badceaf2
Abhinav Kasamsetty <abhinavkasamsetty@gmail.com>
2019-11-26 20:48:50 -0600

    Added number of specialties per type visualization

--- frontend/src/screen/Visualizations/Visualizations.js
+++ frontend/src/screen/Visualizations/Visualizations.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import numDoctorsPerCity from '../../datastore/VisualizationsData/numDoctorsPerCity.json';
+import numSpecialtiesPerType from '../../datastore/VisualizationsData/specialtiesPerType.json';
 import BarChart from '../../component/Graphs/BarChart';
+import PieChart from '../../component/Graphs/PieChart';
 
 function Visualizations(props) {
   return (
     <div style={{ textAlign: 'center', padding: '3em' }}>
       <h1>Visualizations</h1>
+      <br />
+      <br />
       <h2>Number of Doctors Per City</h2>
       <BarChart data={numDoctorsPerCity} xAttr="name" yAttr="numDoctors" />
+      <h2>Number of Specialties Per Type</h2>
+      <PieChart data={numSpecialtiesPerType} />
     </div>
   );
 }

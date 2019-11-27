440e38ee11cfcb88c0e28ff3758ded80a1a8595c
Derrinngo22 <derrinngo@gmail.com>
2019-11-26 19:02:23 -0600

    started visualizations

---
+++ frontend/src/screen/Visualization/Visualization.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default class Visualization extends React.Component{
+    constructor(){
+        console.log("hello");
+        super();
+        this.state = {
+            default: 0
+        }
+    }
+
+    render() {
+        console.log("hello");
+        return(
+
+        )
+    }
+}

0dbe55cf8a2ed85ce8651eb0af958dfe30df8d45
Abhinav Kasamsetty <abhinavkasamsetty@gmail.com>
2019-11-27 17:40:41 -0600

    Generated json data that is required for developer visualizations (fixes #52)

--- frontend/src/datastore/VisualizationsData/GenerateVisualizationsData.js
+++ frontend/src/datastore/VisualizationsData/GenerateVisualizationsData.js
@@ -14,12 +14,16 @@ if (args.length >= 4 && args[2] === '-g') {
   console.log('You must provide -g flag with the value `our` or `developer`.');
 }
 
+const strSecsToIntMin = strSecs => Math.floor(parseInt(strSecs) / 60);
+
 async function generateOurData() {
+  const baseDomain = 'api.doctorsearch.me';
+
   const numDoctorsPerCityData = [];
   const populationVsNumDoctorsData = [];
   const specialtiesPerTypeData = {};
 
-  for await (let city of getInstances('city')) {
+  for await (let city of getInstances(baseDomain, 'city')) {
     numDoctorsPerCityData.push({ name: city.name, numDoctors: city.num_doctors });
     populationVsNumDoctorsData.push({
       population: city.population,
@@ -28,7 +32,7 @@ async function generateOurData() {
   }
   numDoctorsPerCityData.sort((a, b) => b.numDoctors - a.numDoctors);
 
-  for await (let specialty of getInstances('specialty')) {
+  for await (let specialty of getInstances(baseDomain, 'specialty')) {
     if (specialty.category in specialtiesPerTypeData) ++specialtiesPerTypeData[specialty.category];
     else specialtiesPerTypeData[specialty.category] = 1;
   }
@@ -42,15 +46,58 @@ async function generateOurData() {
   fs.writeFile('specialtiesPerType.json', JSON.stringify(specialtiesPerTypeData, null, 2), e => {});
 }
 
-function generateDeveloperData() {
-  console.log('Not implemented yet.');
+async function generateDeveloperData() {
+  const baseDomain = 'api.costlycommute.me';
+
+  const medianWageVsPovertyRateData = [];
+  const avgRentPerCityData = [];
+  const commuteTimesPerCommuteMode = {};
+
+  for await (let city of getInstances(baseDomain, 'city')) {
+    if (city.median_wage && city.poverty_rate) {
+      medianWageVsPovertyRateData.push({
+        medianWage: city.median_wage,
+        povertyRate: city.poverty_rate
+      });
+    }
+    if (city.housing.length > 0) {
+      avgRentPerCityData.push({
+        name: city.name,
+        avgRent: city.housing.reduce((a, b) => a + b.rent, 0) / city.housing.length
+      });
+    }
+  }
+
+  for await (let commute of getInstances(baseDomain, 'commute')) {
+    if (commute.timespan) {
+      if (commute.mode in commuteTimesPerCommuteMode)
+        commuteTimesPerCommuteMode[commute.mode].push(strSecsToIntMin(commute.timespan));
+      else commuteTimesPerCommuteMode[commute.mode] = [strSecsToIntMin(commute.timespan)];
+    }
+  }
+  const avgMinsPerCommuteModeData = Object.entries(commuteTimesPerCommuteMode).map(([k, v]) => ({
+    mode: k,
+    avgMins: v.reduce((a, b) => a + b) / v.length
+  }));
+
+  fs.writeFile(
+    'medianWageVsPovertyRate.json',
+    JSON.stringify(medianWageVsPovertyRateData, null, 2),
+    e => {}
+  );
+  fs.writeFile('avgRentPerCity.json', JSON.stringify(avgRentPerCityData, null, 2), e => {});
+  fs.writeFile(
+    'avgMinsPerCommuteMode.json',
+    JSON.stringify(avgMinsPerCommuteModeData, null, 2),
+    e => {}
+  );
 }
 
-async function* getInstances(model) {
+async function* getInstances(baseDomain, model) {
   let page = 1;
   let data;
   do {
-    const rawData = await fetch(`https://api.doctorsearch.me/api/${model}?page=${page}`);
+    const rawData = await fetch(`https://${baseDomain}/api/${model}?page=${page}`);
     data = await rawData.json();
     for (let obj of data.objects) {
       yield obj;
